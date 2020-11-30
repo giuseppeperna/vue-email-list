35b9d779d541e6d4262ecae8af59736dc381c550
Giuseppe Perna <giuseppeperna.cg@gmail.com>
2020-11-30 17:21:00 +0100

    Vue email list

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
+/**
+@file Stampare a schermo 10 mail provenienti da una API di Boolean
 
+@author Giuseppe Perna <giuseppeperna.cg@gmail.com>
+ */
+
+//  Init Vue object
+const app = new Vue({
+    el:"#app",
+    data: {
+        url:"https://flynn.boolean.careers/exercises/api/random/mail",
+        emails:[],
+    },
+    mounted() {
+        for(let i = 0; i<10; i++) {
+            axios.get(this.url).then((result) => {
+                this.emails.push(result.data.response);
+                console.log(this.emails);
+            });  
+        }
+    },
+})
